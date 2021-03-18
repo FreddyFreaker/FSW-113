9ffaabf19d4f3e470e2cce3c2e66aae696caf79c
FreddyFreaker <jeremiah.fulkerson@bryanuniversity.edu>
2021-03-18 01:43:36 -0500

    Create index.js

---
+++ week1/index.js
@@ -0,0 +1,16 @@
+
+var students = [ 
+
+
+    { name: "John", city: "California" },
+
+
+    { name: "Peter", city: "London"},
+
+
+    { name: "Mathew", city: "Manchester" },
+
+
+    { name: "Jane", city: "Phoenix" }
+
+]
